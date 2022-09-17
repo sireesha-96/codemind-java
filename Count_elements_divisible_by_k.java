6af1aa6fc5b8e78be01effaaa29e79e17ee90a78
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-09-17 13:06:59 +0300

    import java.util.Scanner; class Array {  public static void main(String args[])  {   Scanner sc=new Scanner(System.in);   int n,x[],i,k,c=0;   n=sc.nextInt();   x=new int[n];   k=sc.nextInt();   for ( i=0;i<n;i++)   {    x[i]=sc.nextInt();    }   for( i=0;i<n;i++)   {    if((x[i]%k)==0)     c++;     }   System.out.println(c);   } }

---
+++ Count_elements_divisible_by_k.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Array
+{
+ public static void main(String args[])
+ {
+  Scanner sc=new Scanner(System.in);     
+  int n,x[],i,k,c=0;
+  n=sc.nextInt();
+  x=new int[n];
+  k=sc.nextInt();
+  for ( i=0;i<n;i++)
+  {
+   x[i]=sc.nextInt();
+   }
+  for( i=0;i<n;i++)
+  {
+   if((x[i]%k)==0)
+    c++;
+    }
+  System.out.println(c);
+  }
+}
