401576b02d0b98acc84068382828136be58aa088
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-09-17 13:13:44 +0300

    import java.util.Scanner; class Array1 {  public static void main(String args[])  {   Scanner sc=new Scanner(System.in);   int n,i,x[],count=0;   n=sc.nextInt();   x=new int[n];   for ( i=0;i<n;i++)    x[i]=sc.nextInt();   for( i=1;i<n-1;i++)   {    if(x[i]>=x[i+1])     {      System.out.println("no");      System.exit(0);      }    }    System.out.println("yes");  } }

---
+++ Ascending_array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Array1
+{
+ public static void main(String args[])
+ {
+  Scanner sc=new Scanner(System.in);
+  int n,i,x[],count=0;
+  n=sc.nextInt();
+  x=new int[n];
+  for ( i=0;i<n;i++)
+   x[i]=sc.nextInt();
+  for( i=1;i<n-1;i++)
+  {
+   if(x[i]>=x[i+1])
+    {
+     System.out.println("no");
+     System.exit(0);
+     }
+   }
+   System.out.println("yes");
+ }
+}
