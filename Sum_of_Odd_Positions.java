fb952a5302a7fcbc83046e1b0cdadd42030e88d9
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-09-22 16:29:36 +0300

    import java.util.Scanner; class ArrayDemo3 {   public static void main(String args[])    {          Scanner sc=new Scanner(System.in);          int x[],i,n,sum=0; 		 n=sc.nextInt();
             x=new int[n];
    
    		 //Reading of an array elements         
             for(i=0;i<n;i++)
               {
                 x[i]=sc.nextInt();
               }
    
             for(i=1;i<n;i++)
              {
                 if(i%2!=0)
                 {
    			   sum=sum+x[i];
                 }
              }
    			   System.out.println(sum);
         }
    }

---
+++ Sum_of_Odd_Positions.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+class ArrayDemo3
+{
+  public static void main(String args[])
+   {
+         Scanner sc=new Scanner(System.in);
+         int x[],i,n,sum=0;
+		 n=sc.nextInt();
+
+         x=new int[n];
+
+		 //Reading of an array elements         
+         for(i=0;i<n;i++)
+           {
+             x[i]=sc.nextInt();
+           }
+
+         for(i=1;i<n;i++)
+          {
+             if(i%2!=0)
+             {
+			   sum=sum+x[i];
+             }
+          }
+			   System.out.println(sum);
+     }
+}
