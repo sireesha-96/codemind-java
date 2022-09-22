6cb7ac08acb082e2bdbc94fb810653a7615e1406
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-09-22 11:30:46 +0300

    import java.util.Scanner; class Odd {     public static void main(String args[])     {         int n,x[],i;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(i=n-1;i>=0;i--)         {             if(x[i]%2!=0)             {              System.out.println(i);              break;             }         }     } }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Odd
+{
+    public static void main(String args[])
+    {
+        int n,x[],i;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=n-1;i>=0;i--)
+        {
+            if(x[i]%2!=0)
+            {
+             System.out.println(i);
+             break;
+            }
+        }
+    }
+}
