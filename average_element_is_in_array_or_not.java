ab370f269d2157f9df0c37cc5acb734783b1ee85
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-09-22 18:13:11 +0300

    import java.util.Scanner; class ArrayDemo3 {   public static void main(String args[])    {          Scanner sc=new Scanner(System.in);          int x[],i,n,av=1,sum=0; 		 n=sc.nextInt();
             x=new int[n];
             for(i=0;i<n;i++)
               {
                 x[i]=sc.nextInt();
               }
    
             for(i=0;i<n;i++)
              {
                 sum=sum+x[i];
                 av=sum/n;
                 if(av==i)
                 {
    			   System.out.print("True");
    			   break;
                 }
                 else
                  {
                  System.out.println("False");
                  break;
                  }
               }
         }
    }

--- average_element_is_in_array_or_not.java
+++ average_element_is_in_array_or_not.java
@@ -4,32 +4,29 @@ class ArrayDemo3
   public static void main(String args[])
    {
          Scanner sc=new Scanner(System.in);
-         int x[],i,n,sum=0,av=1;
+         int x[],i,n,av=1,sum=0;
 		 n=sc.nextInt();
 
          x=new int[n];
-
-		 //Reading of an array elements         
          for(i=0;i<n;i++)
            {
              x[i]=sc.nextInt();
            }
-           for(i=0;i<n;i++)
+
+         for(i=0;i<n;i++)
+          {
+             sum=sum+x[i];
+             av=sum/n;
+             if(av==i)
              {
-              sum=sum+x[i];
-              av=sum/n;
-              if(av==i)
+			   System.out.print("True");
+			   break;
+             }
+             else
               {
-               System.out.println("True");
-               break;
+              System.out.println("False");
+              break;
               }
-              else
-               {
-               System.out.println("False");
-               break;
-                }
-                 
-          }
-         
+           }
      }
 }
