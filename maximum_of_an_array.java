cabb1fa2910221fe7a8f7d8160e86e37c0669c6e
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-09-23 03:03:37 +0300

    import java.util.Scanner; class ArrayDemo3 {   public static void main(String args[])    {          Scanner sc=new Scanner(System.in);          int x[],i,n,max=0; 		 n=sc.nextInt();
             x=new int[n];
    
    		      
             for(i=0;i<n;i++)
                 x[i]=sc.nextInt();
                 max=x[0];
             for(i=0;i<n;i++)
              {
                  if(x[i]>max)
                   max=x[i];
               }
    		 System.out.println(max);
         }
    }

---
+++ maximum_of_an_array.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class ArrayDemo3
+{
+  public static void main(String args[])
+   {
+         Scanner sc=new Scanner(System.in);
+         int x[],i,n,max=0;
+		 n=sc.nextInt();
+
+         x=new int[n];
+
+		      
+         for(i=0;i<n;i++)
+             x[i]=sc.nextInt();
+             max=x[0];
+         for(i=0;i<n;i++)
+          {
+              if(x[i]>max)
+               max=x[i];
+           }
+		 System.out.println(max);
+     }
+}
