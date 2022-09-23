20dc5d53fb70f3adea32097d345eef6d68460734
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-09-23 12:30:07 +0300

    import java.util.Scanner; class Array1 {  public static void main(String args[])  {   Scanner sc=new Scanner(System.in);   int n,i,x[],sum=0,count=0;   float av;   n=sc.nextInt();   x=new int[n];   for ( i=0;i<n;i++)    x[i]=sc.nextInt();   for( i=0;i<n;i++)   {     sum=sum+x[i];   }    av=sum/n;    for(i=0;i<n;i++)    {    if(x[i]>=av)     count++;    }     System.out.println(count);  } }

---
+++ elements_greater_than_average.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Array1
+{
+ public static void main(String args[])
+ {
+  Scanner sc=new Scanner(System.in);
+  int n,i,x[],sum=0,count=0;
+  float av;
+  n=sc.nextInt();
+  x=new int[n];
+  for ( i=0;i<n;i++)
+   x[i]=sc.nextInt();
+  for( i=0;i<n;i++)
+  {
+    sum=sum+x[i];
+  }
+   av=sum/n;
+   for(i=0;i<n;i++)
+   {
+   if(x[i]>=av)
+    count++;
+   }
+    System.out.println(count);
+ }
+}
