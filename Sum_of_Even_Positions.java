c882a5c7d5f4407aad1a2b3afe26ca30cd4a0c2b
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-09-23 12:36:55 +0300

    import java.util.Scanner; class Array1 {  public static void main(String args[])  {   Scanner sc=new Scanner(System.in);   int n,i,x[],sum=0;   n=sc.nextInt();   x=new int[n];   for ( i=0;i<n;i++)    x[i]=sc.nextInt();   for( i=0;i<n;i++)   {    if(i%2==0)      sum=sum+x[i];    }    System.out.println(sum);  } }

---
+++ Sum_of_Even_Positions.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Array1
+{
+ public static void main(String args[])
+ {
+  Scanner sc=new Scanner(System.in);
+  int n,i,x[],sum=0;
+  n=sc.nextInt();
+  x=new int[n];
+  for ( i=0;i<n;i++)
+   x[i]=sc.nextInt();
+  for( i=0;i<n;i++)
+  {
+   if(i%2==0)
+     sum=sum+x[i];
+   }
+   System.out.println(sum);
+ }
+}
