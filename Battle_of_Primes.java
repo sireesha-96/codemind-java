d2ddb8c289773dc38e47f26527062520463914fe
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-10-07 04:19:03 +0300

    import java.util.Scanner; class Prime {     public static boolean prime(int n)     {         int c=0,i;         for(i=2;i<=(int)Math.sqrt(n);i++)         {             if(n%i==0)              c++;         }         if(c==0)          return true;         else          return false;     }     public static void main(String args[])     {         int s,r=0,n1,n2;         Scanner sc=new Scanner(System.in);         n1=sc.nextInt();         n2=sc.nextInt();         s=n1+n2;         for(int i=s+1; ;i++)         {             r++;             if(prime(i))              break;         }         System.out.println(r);     } }

--- Battle_of_Primes.java
+++ Battle_of_Primes.java
@@ -3,30 +3,30 @@ class Prime
 {
     public static boolean prime(int n)
     {
-        int count=0,i;
+        int c=0,i;
         for(i=2;i<=(int)Math.sqrt(n);i++)
         {
             if(n%i==0)
-             count++;
+             c++;
         }
-        if(count==0)
+        if(c==0)
          return true;
         else
          return false;
-        }
-        public static void main(String args[])
-        {
-            int s,c=0,n1,n2;
-            Scanner sc=new Scanner(System.in);
-            n1=sc.nextInt();
-            n2=sc.nextInt();
-            s=n1+n2;
-            for(int i=s+1; ;i++)
-            {
-                c++;
-                if(prime(i))
-                 break;
-            }
-                System.out.println(c);
-        }
     }
+    public static void main(String args[])
+    {
+        int s,r=0,n1,n2;
+        Scanner sc=new Scanner(System.in);
+        n1=sc.nextInt();
+        n2=sc.nextInt();
+        s=n1+n2;
+        for(int i=s+1; ;i++)
+        {
+            r++;
+            if(prime(i))
+             break;
+        }
+        System.out.println(r);
+    }
+}
