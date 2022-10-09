7300f37c7547cfdb96116ac6b579210ce68e5d2c
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-10-09 17:03:32 +0300

    import java.util.Scanner; class Unique {  public static void main(String args[])  {   Scanner sc=new Scanner(System.in);   int n,i,j,x[],count=0,flag=0;   n=sc.nextInt();   x=new int[n];   for ( i=0;i<n;i++)    x[i]=sc.nextInt();   for( i=0;i<n;i++)   {    if (x[i]!=-999)     {      count=1;      for(j=0;j<n;j++)      {       if(x[i]==x[j]&&i!=j)       {        count++;        x[j]=-999;        }       }       System.out.print(x[i] +" "+ count+" ");      }  }  } }

---
+++ Element_and_Count.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class Unique
+{
+ public static void main(String args[])
+ {
+  Scanner sc=new Scanner(System.in);
+  int n,i,j,x[],count=0,flag=0;
+  n=sc.nextInt();
+  x=new int[n];
+  for ( i=0;i<n;i++)
+   x[i]=sc.nextInt();
+  for( i=0;i<n;i++)
+  {
+   if (x[i]!=-999)
+    {
+     count=1;
+     for(j=0;j<n;j++)
+     {
+      if(x[i]==x[j]&&i!=j)
+      {
+       count++;
+       x[j]=-999;
+       }
+      }
+      System.out.print(x[i] +" "+ count+" ");
+     }
+ }
+ }
+}
