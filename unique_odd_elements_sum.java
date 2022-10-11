1206f0a1a14a973613c6aa2b2e4265fc5f274ad3
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-10-11 18:37:59 +0300

    import java.util.Scanner; class Frequency {
       public static void main(String[] args) {
            // TODO Auto-generated method stub
            int n,i,j,x[],count=0,sum=0;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            x=new int[n];
            for(i=0;i<n;i++)
                x[i]=sc.nextInt();
            for(i=0;i<n;i++)
              {        
                if(x[i]!=-99)
                {
                  for(j=0;j<n;j++)
                  {
                    if(x[i]==x[j] && i!=j)
                    {
                       
                        x[j]=-99;
                    }
                  }
                if(x[i]%2==1)
                 sum=sum+x[i];
                    }
                }
                System.out.println(sum);
            }
            
    }

--- unique_odd_elements_sum.java
+++ unique_odd_elements_sum.java
@@ -1,32 +1,33 @@
 import java.util.Scanner;
-class Unique
-{
- public static void main(String args[])
- {
-  Scanner sc=new Scanner(System.in);
-  int n,i,j,x[],sum=0;
-  n=sc.nextInt();
-  x=new int[n];
-  for ( i=0;i<n;i++)
-   x[i]=sc.nextInt();
-  for( i=0;i<n;i++)
-  {
-   if (x[i]!=-999)
-    {
-     for(j=0;j<n;j++)
-     {
-      if(x[i]==x[j]&&i!=j)
-      {
-       x[j]=-999;
-       }
-      }
-     if(x[i]%2!=0)
-     {
-         sum=sum+x[i];
-     }
-    }
-  }
-  System.out.println(sum);
- }
+class Frequency {
+
+
+
+   public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n,i,j,x[],count=0,sum=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+            x[i]=sc.nextInt();
+        for(i=0;i<n;i++)
+          {        
+            if(x[i]!=-99)
+            {
+              for(j=0;j<n;j++)
+              {
+                if(x[i]==x[j] && i!=j)
+                {
+                   
+                    x[j]=-99;
+                }
+              }
+            if(x[i]%2==1)
+             sum=sum+x[i];
+                }
+            }
+            System.out.println(sum);
+        }
+        
 }
- 
