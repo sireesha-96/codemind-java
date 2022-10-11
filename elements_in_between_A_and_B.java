ad937de91b66d44e2fecfb68243db1be43fd6a3d
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-10-11 18:58:00 +0300

    import java.util.Scanner; class Frequency {
       public static void main(String[] args) {
            // TODO Auto-generated method stub
            int n,i,j,x[],a,b,count=0;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            x=new int[n];
            for(i=0;i<n;i++)
                x[i]=sc.nextInt();
                a=sc.nextInt();
                b=sc.nextInt();
            for(i=0;i<n;i++)
            {   
                if(x[i]>=a&&x[i]<=b)
                {
                    System.out.print(x[i]+" ");
                    count++;
                }
            }
            if(count==0)
             System.out.println("-1");
       }
    }

--- elements_in_between_A_and_B.java
+++ elements_in_between_A_and_B.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
-class Array1
-{
- public static void main(String args[])
- {
-  Scanner sc=new Scanner(System.in);
-  int n,i,x[],a,b,c=0;
-  n=sc.nextInt();
-  x=new int[n];
-  for ( i=0;i<n;i++)
-   x[i]=sc.nextInt();
-   a=sc.nextInt();
-   b=sc.nextInt();
-  for( i=0;i<n;i++)
-  {
-      if(x[i]>=a&&x[i]<=b)
-      {
-       System.out.print(x[i]+" ");
-       c++;
-      }
-  }
-     if(c==0)
-       System.out.println("-1");
+class Frequency {
+
+
+
+   public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n,i,j,x[],a,b,count=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+            x[i]=sc.nextInt();
+            a=sc.nextInt();
+            b=sc.nextInt();
+        for(i=0;i<n;i++)
+        {   
+            if(x[i]>=a&&x[i]<=b)
+            {
+                System.out.print(x[i]+" ");
+                count++;
+            }
+        }
+        if(count==0)
+         System.out.println("-1");
    }
 }
