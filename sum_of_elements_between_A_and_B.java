4ceda8e93bd25367192a2ad434d7cf121cee2710
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-10-11 18:51:18 +0300

    import java.util.Scanner; class Frequency {
       public static void main(String[] args) {
            // TODO Auto-generated method stub
            int n,i,j,x[],a,b,sum=0;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            x=new int[n];
            for(i=0;i<n;i++)
                x[i]=sc.nextInt();
                a=sc.nextInt();
                b=sc.nextInt();
            for(i=0;i<n;i++)
            {  
                if(x[i]>=a&&x[i]<=b)
                {
                    sum=sum+x[i];
                }
            }
            System.out.println(sum);
       }
    }

--- sum_of_elements_between_A_and_B.java
+++ sum_of_elements_between_A_and_B.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
-class Skil1
-{
- public static void main(String args[])
- {
-  Scanner sc=new Scanner(System.in);
-  int n,i,x[],a,b,sum=0;
-  n=sc.nextInt();
-  x=new int[n];
-  for ( i=0;i<n;i++)
-   x[i]=sc.nextInt();
-   a=sc.nextInt();
-   b=sc.nextInt();
-  for( i=0;i<n;i++)
-  {
-   if(x[i]>=a&&x[i]<=b)
-    sum=sum+x[i];
+class Frequency {
+
+
+
+   public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n,i,j,x[],a,b,sum=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+            x[i]=sc.nextInt();
+            a=sc.nextInt();
+            b=sc.nextInt();
+        for(i=0;i<n;i++)
+        {  
+            if(x[i]>=a&&x[i]<=b)
+            {
+                sum=sum+x[i];
+            }
+        }
+        System.out.println(sum);
    }
-  System.out.println(sum);
- }
 }
+            
