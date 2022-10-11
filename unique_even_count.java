862006bddaaf80de1ad5855447e85d99482169b5
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-10-11 18:45:45 +0300

    import java.util.Scanner; class Frequency {
       public static void main(String[] args) {
            // TODO Auto-generated method stub
            int n,i,j,x[],count=0;
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            x=new int[n];
            for(i=0;i<n;i++)
                x[i]=sc.nextInt();
                     for(i=0;i<n;i++)
            {    
                if(x[i]!=-99)
                {
                  for(j=0;j<n;j++)
                  {
                    if(x[i]==x[j] && i!=j)
                    {
                        
                        x[j]=-99;
                    }
                  }
                  if(x[i]%2==0)
                   count++;
                }
            }
            System.out.println(count);
       }
    }

--- unique_even_count.java
+++ unique_even_count.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
-class Unique
-{
- public static void main(String args[])
- {
-  Scanner sc=new Scanner(System.in);
-  int n,i,j,x[],count=0;
-  n=sc.nextInt();
-  x=new int[n];
-  for ( i=0;i<n;i++)
-   x[i]=sc.nextInt();
-  for( i=0;i<n;i++)
-  {
-   if (x[i]!=-999)
-    {
-     for(j=0;j<n;j++)
-     {
-      if(x[i]==x[j]&&i!=j)
-      {
-       x[j]=-999;
-       }
-      }
-     if(x[i]%2==0)
-     {
-       count++;
-      }
-     }
-    }
-   System.out.println(count);
- }
+class Frequency {
+
+
+
+   public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n,i,j,x[],count=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+            x[i]=sc.nextInt();
+                 for(i=0;i<n;i++)
+        {    
+            if(x[i]!=-99)
+            {
+              for(j=0;j<n;j++)
+              {
+                if(x[i]==x[j] && i!=j)
+                {
+                    
+                    x[j]=-99;
+                }
+              }
+              if(x[i]%2==0)
+               count++;
+            }
+        }
+        System.out.println(count);
+   }
 }
