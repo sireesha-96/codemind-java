c45821db03fa14c6facf34b842bd1bf00826342b
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2022-10-12 03:48:29 +0300

    import java.util.Scanner; class Frequency {
       public static void main(String[] args) {
            // TODO Auto-generated method stub
            int n,i,j,x[];
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            x=new int[n];
            for(i=0;i<n;i++)
                x[i]=sc.nextInt();
                     for(i=0;i<n;i++)
            {   
                if(x[i]!=-99)
                {
                  for(j=0;j<n;j++)
                  {
                    if(x[i]==x[j] && i!=j)
                    {
             
                        x[j]=-99;
                    }
                  }
                System.out.print(x[i]+" ");
                }
            }
       }
    }

---
+++ unique_elements.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Frequency {
+
+
+
+   public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n,i,j,x[];
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+            x[i]=sc.nextInt();
+                 for(i=0;i<n;i++)
+        {   
+            if(x[i]!=-99)
+            {
+              for(j=0;j<n;j++)
+              {
+                if(x[i]==x[j] && i!=j)
+                {
+         
+                    x[j]=-99;
+                }
+              }
+            System.out.print(x[i]+" ");
+            }
+        }
+   }
+}
