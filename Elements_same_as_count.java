3ada9e6d49abb3b28f414702da58eec8007f7e28
sireesha-96 <84322529+sireesha-96@users.noreply.github.com>
2023-01-21 13:17:58 +0300

    import java.util.*; import java.util.Map.*; class Sample {     public static void printOutput(int arr[])     {         int c=0;         HashMap<Integer,Integer>hm=new HashMap<>();         for(int i=0;i<arr.length;i++)         {             hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);         }         for(int i=0;i<arr.length;i++)         {             if(arr[i]== (int)hm.get(arr[i]))             {                 System.out.print(arr[i]+" ");                 hm.put(arr[i],0);                 c++;             }          }      if(c==0)          System.out.println("-1");        }
            public static void main(String args[])
            {
            int n,x[];
            Scanner sc=new Scanner(System.in);
            n=sc.nextInt();
            x=new int[n];
            for(int i=0;i<n;i++)
             x[i]=sc.nextInt();
            printOutput(x);
        }
    }

--- Elements_same_as_count.java
+++ Elements_same_as_count.java
@@ -1,43 +1,36 @@
-import java.util.Scanner;
-class Frequency {
-
-
-
-   public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        int n,i,j,x[],count=0,flag=0;
+import java.util.*;
+import java.util.Map.*;
+class Sample
+{
+    public static void printOutput(int arr[])
+    {
+        int c=0;
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]== (int)hm.get(arr[i]))
+            {
+                System.out.print(arr[i]+" ");
+                hm.put(arr[i],0);
+                c++;
+            }
+         }
+     if(c==0)
+         System.out.println("-1");
+       }
+    
+        public static void main(String args[])
+        {
+        int n,x[];
         Scanner sc=new Scanner(System.in);
         n=sc.nextInt();
         x=new int[n];
-        for(i=0;i<n;i++)
-            x[i]=sc.nextInt();
-                 for(i=0;i<n;i++)
-        {    count=1;
-            if(x[i]!=-99)
-            {
-              for(j=0;j<n;j++)
-              {
-                if(x[i]==x[j] && i!=j)
-                {
-                    count++;
-                    x[j]=-99;
-                }
-              }
-              //System.out.println(x[i]+" "+count);
-              if(x[i]==count)
-                {
-                 System.out.print(x[i]+" ");
-                  flag=1;
-                }
-            }
-        }
-         if(flag==0)
-           System.out.println("-1");
-
-
-
-   }
-
-
-
+        for(int i=0;i<n;i++)
+         x[i]=sc.nextInt();
+        printOutput(x);
+    }
 }
